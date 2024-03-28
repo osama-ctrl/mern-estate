7c0cd96be7f444c0a89e1e884d7556f71fb355c6
osama-ctrl <osamakhokhar442@gmail.com>
2024-03-28 23:26:54 +0500

    Create Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import React from "react";
 
 export default function App() {
   return (
-    <div className='text-red-700'>
-      hello
-
-
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signout" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-    
